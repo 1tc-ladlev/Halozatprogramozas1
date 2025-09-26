e392d86a20774af1963cdb051594df3062023b79
1tc-ladlev <ladanyilevente5@gmail.com>
2025-09-26 13:01:00 +0200

    kesz

--- szamolo.py
+++ szamolo.py
@@ -1,38 +1,38 @@
 szamok=[1,1,2,2,4,5,2,1,3]
-# egyes_db=0
-# kettes_db=0
-# harmas_db=0
-# negyes_db=0
-# otos_db=0
+egyes_db=0
+kettes_db=0
+harmas_db=0
+negyes_db=0
+otos_db=0
 
-# for szam in szamok:
-#     if szam==1:
-#         egyes_db+=1
-#     elif szam==2:
-#         kettes_db+=1
-#     elif szam==3:
-#         harmas_db+=1
-#     elif szam==4:
-#         negyes_db+=1
-#     elif szam==5:
-#         otos_db+=1
+for szam in szamok:
+    if szam==1:
+        egyes_db+=1
+    elif szam==2:
+        kettes_db+=1
+    elif szam==3:
+        harmas_db+=1
+    elif szam==4:
+        negyes_db+=1
+    elif szam==5:
+        otos_db+=1
 
-# print(f"Az 1-es számból ennyi van {egyes_db}")
-# print(f"Az 2-es számból ennyi van {kettes_db}")
-# print(f"Az 3-as számból ennyi van {harmas_db}")
-# print(f"Az 4-es számból ennyi van {negyes_db}")
-# print(f"Az 5-ös számból ennyi van {otos_db}")
+print(f"Az 1-es számból ennyi van {egyes_db}")
+print(f"Az 2-es számból ennyi van {kettes_db}")
+print(f"Az 3-as számból ennyi van {harmas_db}")
+print(f"Az 4-es számból ennyi van {negyes_db}")
+print(f"Az 5-ös számból ennyi van {otos_db}")
 
-# for i in range(1,6):
-#     print(f"{i}-ből van {szamok.count(i)}")
+for i in range(1,6):
+    print(f"{i}-ből van {szamok.count(i)}")
 
-# darab=[0,0,0,0,0]
+darab=[0,0,0,0,0]
 
-# for szam in szamok:
-#     darab[szam-1]+=1
+for szam in szamok:
+    darab[szam-1]+=1
 
-# print(darab)
+print(darab)
 
-# for i in range(len(darab)):
-#     print(f"{i+1}-ból van {darab[i]+1}")
+for i in range(len(darab)):
+    print(f"{i+1}-ból van {darab[i]+1}")
 
